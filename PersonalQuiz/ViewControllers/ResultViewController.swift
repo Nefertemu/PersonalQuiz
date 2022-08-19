b9cc7697bf8d60ffb759c91ec187c00bc1a50090
Bogdan Anishchenkov <bogdaggi@gmail.com>
2022-08-19 20:56:16 +0300

    Found most frequent animal

--- PersonalQuiz/ViewControllers/ResultViewController.swift
+++ PersonalQuiz/ViewControllers/ResultViewController.swift
@@ -10,21 +10,55 @@ import UIKit
 class ResultViewController: UIViewController {
     
     // 1. Передать массив с ответами на экран с результатами - Done
-    // 2. Определить наиболее часто встречающийся тип животного
+    // 2. Определить наиболее часто встречающийся тип животного - Done
     // 3. Отобразить результаты в соотвствии с этим животным
     // 4. Избавиться от кнопки возврата назад на экране результатов - Done
     
     var answers: [Answer] = []
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         navigationItem.setHidesBackButton(true, animated: false)
     }
- 
+    
     @IBAction func doneButtonPressed(_ sender: UIBarButtonItem) {
         dismiss(animated: true)
+        findTheMostFrequentAnimal()
         print(answers)
     }
     
+    private func findTheMostFrequentAnimal() -> AnimalType {
+        var dogCount = 0
+        var catCount = 0
+        var rabbitCount = 0
+        var turtleCount = 0
+        var mostFrequentAnimal: AnimalType = .dog
+        
+        for animal in answers {
+            switch animal.type {
+            case .dog:
+                dogCount += 1
+            case .cat:
+                catCount += 1
+            case .rabbit:
+                rabbitCount += 1
+            case .turtle:
+                turtleCount += 1
+            }
+        }
+        
+        if dogCount > catCount, dogCount > rabbitCount, dogCount > turtleCount {
+            mostFrequentAnimal = .dog
+        } else if catCount > dogCount, catCount > rabbitCount, catCount > turtleCount {
+            mostFrequentAnimal = .cat
+        } else if rabbitCount > dogCount, rabbitCount > catCount, rabbitCount > turtleCount {
+            mostFrequentAnimal = .rabbit
+        } else if turtleCount > dogCount, turtleCount > catCount, turtleCount > rabbitCount {
+            mostFrequentAnimal = .turtle
+        }
+        
+        print(mostFrequentAnimal)
+        return mostFrequentAnimal
+    }
 }
